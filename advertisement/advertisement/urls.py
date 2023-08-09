f84c724d72962cc0c07699f5bd9a6f40bc0f55c3
Sergey <137311616+vorontsovso-1@users.noreply.github.com>
2023-08-09 22:22:12 +0300

    Update urls.py

--- advertisement/advertisement/urls.py
+++ advertisement/advertisement/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index
-from .views import lesson
+from .views import index, top_sellers
+# from .views import lesson
 
 urlpatterns = [
-    path("",index),
-    path('lesson_4/',lesson)
+    path("",index, name="main-page"),
+    path('top-sellers/',top_sellers,name="top-sellers")
+    # path('lesson_4/',lesson)
 ]
