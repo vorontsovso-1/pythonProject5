ad40ad25db333c799ff4372d5820f58e972ccc7d
Sergey <137311616+vorontsovso-1@users.noreply.github.com>
2023-08-09 22:25:40 +0300

    Update views.py

--- advertisement/advertisement/views.py
+++ advertisement/advertisement/views.py
@@ -2,14 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("""
-        <h1>ВСЕМ ПРИВЕТ!</h1>
-        <a class="navbar-brand" href="lesson_4/">
-            ДОМАШКА ДЛЯ ЗАДАНИЯ №4
-        </a> 
-    """)
+#     return HttpResponse("""
+#         <h1>Все супер</h1>
+#         <a class="navbar-brand" href="lesson_4/">
+#             Домашняя работа 4 занятия
+#         </a>
+#     """)
+#
+# def lesson(request):
+#     return HttpResponse("""
+#         <h1> Вот и домашка </h1>
+#     """)
+    return render(request, 'index.html')
 
-def lesson(request):
-    return HttpResponse("""
-        <h1> СОДЕРЖИМОЕ </h1>
-    """)
+def top_sellers(request):
+    return render(request, "top-sellers.html")
