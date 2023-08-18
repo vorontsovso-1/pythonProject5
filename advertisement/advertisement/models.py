c12d8f1188e40735b44588927053d890d0ab42ac
Sergey <137311616+vorontsovso-1@users.noreply.github.com>
2023-08-18 11:32:43 +0300

    Update models.py

--- advertisement/advertisement/models.py
+++ advertisement/advertisement/models.py
@@ -1,67 +1,85 @@
 from django.db import models
 from django.contrib import admin
+from django.utils import timezone
+from django.utils.html import format_html
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 
 class Advertisement(models.Model):
-    title=models.CharField(
+    title = models.CharField(
         max_length=100,
-        verbose_name='Название',
-
-
+        verbose_name="Название",
     )
 
-    description=models.TextField(
-        verbose_name='Описание',
+    description = models.TextField(
+        verbose_name="Описание"
     )
-    price=models.DecimalField(
-        verbose_name='Цена',
+
+    price = models.DecimalField(
+        verbose_name="Цена",
         max_digits=10,
         decimal_places=2
     )
 
-    auction=models.BooleanField(
-        verbose_name='Торг',
+    auction = models.BooleanField(
+        verbose_name="Торг",
         default=False
     )
 
-    created_at=models.DateTimeField(
+    created_at = models.DateTimeField(
         auto_now_add=True,
         verbose_name="Дата публикации"
-
     )
-    updated_at=models.DateTimeField(
-        auto_now_add=True,
-        verbose_name='Дата редактирования'
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name="Дата редоктирования"
+    )
+    user = models.ForeignKey(
+        to=User,
+        verbose_name="пользователь",
+        on_delete=models.CASCADE
+    )
+    image = models.ImageField(
+        upload_to="advertisements/",
+        verbose_name="изображение",
     )
 
-    @admin.display(description='дата создания')
+    @admin.display(description='Дата создания')
     def created_date(self):
-        from django.utils import timezone
-        from django.utils.html import format_html
-
         if self.created_at.date() == timezone.now().date():
-            created_time= self.created_at.time().strftime('%H:%M:%S')
+            created_time = self.created_at.time().strftime("%H:%M:%S")
             return format_html(
-                '<span style="color: green; font-weight: bold;">Сегодня в {}</span>',created_time
+                '<span style="color: green; font-weight: bold;">Сегодня в {}</span>', created_time
             )
-        return self.created_at.strftime('%d.%m.%Y в %H:%M:%S')
 
-    @admin.display(description='дата редактирования')
+        return self.created_at.strftime("%d.%m.%y в %H:%M")
+
+    @admin.display(description="Дата обновления")
     def updated_date(self):
-        from django.utils import timezone
-        from django.utils.html import format_html
-
         if self.updated_at.date() == timezone.now().date():
-            created_time = self.updated_at.time().strftime('%H:%M:%S')
+            updated_time = self.updated_at.time().strftime("%H:%M:%S")
             return format_html(
-                '<span style="color: blue; font-weight: bold;">Сегодня в {}</span>', created_time
+                '<span style="color: blue; font-weight": bold;>Обновлено в {}</span>', updated_time
             )
-        return self.updated_at.strftime('%d.%m.%Y в %H:%M:%S')
+        return self.updated_at.strftime("%d.%m.%y в %H:%M")
 
-    class Admin:
-        list_display = ["id", "title"]
+    @admin.display(description="Фотография")
+    def show_image(self):
+        if self.image:
+            return format_html(
+                '<img src={} style="width: 40px; height: 40px;"/>', self.image.url
+            )
+        else:
+            return format_html(
+                '<span style="color: red;">Нету фото<span/>'
+            )
 
     def __str__(self):
-        return "%s    %s"%(self.id,self.title)
+        return f"id {self.id} title {self.title} price {self.price}"
 
     class Meta:
-        db_table='advertisement'
+        db_table = "advertisement"
+        verbose_name = "Объявление"
+        verbose_name_plural = "Объявления"
